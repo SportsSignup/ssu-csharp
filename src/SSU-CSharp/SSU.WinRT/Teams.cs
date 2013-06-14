b5e0766dbf616b8ef266b101d7eba44db96dfb0b
unknown <francis@nibbler.upstatespors.com>
2013-06-13 23:29:41 -0400

    Added team query to WinRT

---
+++ src/SSU-CSharp/SSU.WinRT/Teams.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SSU.Model;
+using Windows.Foundation;
+
+namespace SSU
+{
+    public sealed partial class SSURestClient
+    {
+        public IAsyncOperation<IEnumerable<Team>> TeamsByDivisionIdAsync(int divisionId)
+        {
+            return AsyncInfo.Run(ct => GetByDivisionIdInteral(divisionId));
+        }
+
+        private async Task<IEnumerable<Team>> GetByDivisionIdInteral(int divisionId)
+        {
+            var url = BaseUrl + "/Teams/ByDivisionId/{Id}".Replace("{Id}", divisionId.ToString());
+            var response = await new HttpClient(handler).GetAsync(url);
+
+            string responseString = await response.Content.ReadAsStringAsync();
+            // parse to json
+            var uncastedResult =
+                JsonConvert.DeserializeObject(responseString, typeof(IEnumerable<Team>));
+            if (uncastedResult is IEnumerable<Team>)
+            {
+                return uncastedResult as IEnumerable<Team>;
+            } 
+            return new List<Team>();
+        }
+    }
+}
