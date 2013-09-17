9857f43409f1c4cb73abd4808251978339ec528b
Francis Spor <francis@upstatespors.com>
2013-09-17 14:41:39 -0400

    Something was messed up about my package script, but should be better now

--- package.cmd
+++ package.cmd
@@ -12,8 +12,8 @@ if not exist download\package\sportssignup-winrt\lib mkdir download\package\spor
 tools\ilmerge.exe /lib:src\SSU-CSharp\SSU.Api\bin\Release /internalize /ndebug /v2 /out:download\SSU.Api.dll SSU.Api.dll RestSharp.dll
 tools\ilmerge.exe /lib:src\SSU-CSharp\SSU.WinRT\bin\Release /internalize /ndebug /v2 /out:download\SSU.winmd SSU.winmd RestRT.winmd
 
-copy src\SSU-CSharp\SSUApi.Api\bin\Release\*.* download
-copy src\SSU-CSharp\SSU.WinRT\bin\Release\*.* download
+copy src\SSU-CSharp\SSU.Api\bin\Release\*.* download\package\sportssignup\lib
+copy src\SSU-CSharp\SSU.WinRT\bin\Release\*.* download\package\sportssignup-winrt\lib
 copy LICENSE.txt download
 
 tools\nuget.exe pack SportsSignup.nuspec -BasePath download\package\sportssignup -o download
